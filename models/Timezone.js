d135f0bb7c7754490eef2594c9bdee4f0142372b
Daniela Vazquez <danielavazquez95@gmail.com>
2021-02-26 00:17:49 -0300

    REFACTOR: model name

--- models/Timezone.js
+++ models/Timezone.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, type) => {
-    return sequelize.define('operation', {
+    return sequelize.define('Timezones', {
         id:{
             type: type.INTEGER,
             primaryKey: true,
