bb9bfd03f1ceef6b3aab04450e47f6ca17f7842e
Daniela Vazquez <danielavazquez95@gmail.com>
2021-02-27 18:09:18 -0300

    ADD: Environment Variables

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
+
 const TimeZoneModel = require('./models/Timezone');
 
-const sequelize = new Sequelize('KuUHuZ5yWR', 'KuUHuZ5yWR', '6vXuwPYGnc', {
+const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: 'remotemysql.com',
     dialect: 'mysql'
 } );
 
 const Timezones = TimeZoneModel(sequelize, Sequelize);
 
-sequelize.sync({force: true})
+sequelize.sync({force: false})
     .then(() => {
         console.log('Synchronized tables')
     });
--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const  axios =  require('axios');
 const { Timezones } = require('../db');
 
-
 router.get('/timezones', async (req, res) => {
 
     try {
         const response = await axios.get('https://worldtimeapi.org/api/timezone');
         const objectData = response.data.map( item => ({name: item}));
-        res.json(objectData)
-      } catch (error) {
+        res.json(objectData);
+
+      } catch (err) {
         res.status(413).json({ 'error': err.message}); 
     }
 
@@ -19,8 +19,9 @@ router.get('/timezones/:area/:location', async (req, res) => {
 
   try {
       const response = await axios.get(`https://worldtimeapi.org/api/timezone/${req.params.area}/${req.params.location}`);
-      res.json(response.data)
-    } catch (error) {
+      res.json(response.data);
+
+    } catch (err) {
       res.status(413).json({ 'error': err.message}); 
   }
 
@@ -30,46 +31,64 @@ router.get('/timezones/:area/:location/:region', async (req, res) => {
 
     try {
         const response = await axios.get(`https://worldtimeapi.org/api/timezone/${req.params.area}/${req.params.location}/${req.params.region}`);
-        console.log(response);
-        res.json(response.data)
-      } catch (error) {
+        res.json(response.data);
+
+      } catch (err) {
         res.status(413).json({ 'error': err.message}); 
     }
 
 });
 
 router.post('/timezones/dbupload', async (req, res) => {
+  
+  try{
 
-  let timezones = await Timezones.findAll({
-    where: {name : req.body.name }
-  });
+    let timezones = await Timezones.findAll({
+      where: {name : req.body.name }
+    });
 
-  if(timezones.length > 0) {
-    res.json({message: 'The name already exists'});
-    throw new Error ('The name already exists');
+    if(timezones.length > 0) {
+      res.json({message: 'The name already exists'});
+      throw new Error ('The name already exists');
+    }
+
+    timezones = await Timezones.create(req.body);
+    res.json(timezones);
+
+  } catch(err) {
+    res.status(413).json({ 'error': err.message}); 
   }
-
-  timezones = await Timezones.create(req.body);
-  res.json(timezones);
 
 });
 
 router.get('/timezones/dbload', async (req, res) => {
 
+  try {
     const timezones = await Timezones.findAll({
       order : [ 
           [ 'createdAt' ,  'asc' ] 
         ] 
     });
+    
      res.json(timezones);
+
+  } catch (err) {
+    res.status(413).json({ 'error': err.message}); 
+  }
 });
 
 router.delete('/timezones/dbdelete/:id', async (req, res) =>{
 
-    await Timezones.destroy({
+  try{
+     const timezones = await Timezones.destroy({
       where: {id: req.params.id}
     });
     res.json({success: 'It was deleted'});
+
+  } catch (err) {
+    res.status(413).json({ 'error': err.message}); 
+  }
+
 });
 
 
